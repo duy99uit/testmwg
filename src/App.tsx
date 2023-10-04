79f1698e751120170d2522c622cd9f06506029ca
Doan The Duy <duy99.uit@gmail.com>
2023-10-04 14:08:49 +0700

    update code

--- src/App.tsx
+++ src/App.tsx
@@ -2,42 +2,10 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  // const [textInput, setTextInput] = useState("");
-
-  // const handleChangeVale = (
-  //   event: React.InputHTMLAttributes<HTMLInputElement>
-  // ) => {
-  //   console.log("handleChangeVale", event.target.value);
-  //   setTextInput(event.target.value);
-  // };
-
-  // const renderListData = useMemo(() => {
-  //   if (textInput.trim().length === 0) return listData;
-  //   const _removeVietnameseTones =
-  //     removeVietnameseTones(textInput).toLocaleUpperCase();
-
-  //   return listData.filter((item) => {
-  //     return removeVietnameseTones(item?.name)
-  //       .toLocaleUpperCase()
-  //       .includes(_removeVietnameseTones);
-  //   });
-  // }, [textInput]);
-
-  // const data: any = useMemo(() => {
-  //   return loopArr(fakeArr);
-  // }, []);
-
   return (
     <React.Fragment>
       <div>
-        {/* <div>{data?.length}</div> */}
         <a href="loyalty://loyalty/LuckyWheelGame">Mở app loyalty</a>
-        {/* <input value={textInput} type="text" onChange={handleChangeVale} />
-        <div>
-          {renderListData?.map((item) => {
-            return <p key={item.id}>{item.name}</p>;
-          })}
-        </div> */}
       </div>
     </React.Fragment>
   );
---
+++ src/screen/Test.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Test() {
+  return (
+    <React.Fragment>
+      <div>
+        <p>Duy test something</p>
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default Test;
