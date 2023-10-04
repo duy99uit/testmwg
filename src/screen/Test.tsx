c4a0d647d0b6b50cef0b40b4a2f4ebe4ba021377
Doan The Duy <duy99.uit@gmail.com>
2023-10-04 14:58:09 +0700

    add story book

--- src/screen/Test.tsx
+++ src/screen/Test.tsx
@@ -1,10 +1,16 @@
 import React from "react";
+import useToggle from "../hook/useToggle";
 
 function Test() {
+  const [value, toggleValue] = useToggle(false);
   return (
     <React.Fragment>
       <div>
         <p>Duy test something</p>
+        <div>{value.toString()}</div>
+        <button onClick={toggleValue}>Toggle</button>
+        <button onClick={() => toggleValue(true)}>Make True</button>
+        <button onClick={() => toggleValue(false)}>Make False</button>
       </div>
     </React.Fragment>
   );
